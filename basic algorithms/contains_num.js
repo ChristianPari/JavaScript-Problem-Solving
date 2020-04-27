0dc8e14967231ee900a5949060a16b8f5dc8ca5e
Christian Pari <ChristianPari@outlook.com>
2020-04-27 15:15:10 -0400

    Basic Algorithm

---
+++ basic algorithms/contains_num.js
@@ -0,0 +1,14 @@
+/*
+Write a regular expression that matches a string if it contains at least one digit.
+
+Example Output:
+"c8" ➞ true
+
+"23cc4" ➞ true
+
+"abwekz" ➞ false
+
+"sdfkxi" ➞ false
+*/
+
+let regex = /\d/;
