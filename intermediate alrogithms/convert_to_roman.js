be0d622cfd75477b785da5a6a584e6d4df89df16
Christian Pari <ChristianPari@outlook.com>
2020-06-02 08:45:49 -0400

    Updated Code

--- intermediate alrogithms/convert_to_roman.js
+++ intermediate alrogithms/convert_to_roman.js
@@ -5,12 +5,12 @@ All roman numerals answers should be provided in upper-case.
 
 function convertToRoman(num) {
 
-    const romans = ['I', 'IV', 'V', 'IX', 'X', 'XLIX', 'L', 'XCIX', 'C', 'CD', 'D', 'CM', 'M'],
-        regNums = [1, 4, 5, 9, 10, 49, 50, 99, 100, 400, 500, 900, 1000];
+    const romans = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'XLIX', 'L', 'XC', 'XCIX', 'C', 'CD', 'D', 'CM', 'M'],
+        regNums = [1, 4, 5, 9, 10, 40, 49, 50, 90, 99, 100, 400, 500, 900, 1000];
 
     let answer = "";
 
-    for (let idx = regNums.length; idx > 0; idx--) {
+    for (let idx = regNums.length; idx >= 0; idx--) {
 
         while (regNums[idx] <= num) {
             // at least one of the numbers in the regNums array will be less than the 'num' parameter so this will always be ran
@@ -27,4 +27,4 @@ function convertToRoman(num) {
 
 }
 
-console.log(convertToRoman(1250));
+console.log(convertToRoman(97));
