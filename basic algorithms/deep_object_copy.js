77a1e538c63feb85bd11aa1ccf6cc5b29759fb1a
Christian Pari <ChristianPari@outlook.com>
2020-07-07 19:38:37 -0400

    deep object copy

--- basic algorithms/deep_object_copy.js
+++ basic algorithms/deep_object_copy.js
@@ -1,16 +1,7 @@
 // Return a deep copy of an object using a function
-function deepCopy(obj) {
-
-    const { a, b, c, f } = obj,
-    newObj = { a, b, c, f };
-
-    return newObj;
-
-};
-
-deepCopy({
+const test = {
     a: "test",
     b: 3,
     c: { d: "testing", e: 7 },
-    f: { nested: { deep: "this is deep" } }
-});
+    f: { nested: { deep: "this is deep", fun() { console.log('hi'); } } }
+};
